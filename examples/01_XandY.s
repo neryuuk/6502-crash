15f696a138b30ce17606207d5f4702332859238b
Nelson Antunes <966712+neryuuk@users.noreply.github.com>
2024-05-08 20:16:44 -0300

    comments

--- examples/01_XandY.s
+++ examples/01_XandY.s
@@ -2,25 +2,16 @@
 .segment "CODE"
 
 .proc Main
-  ; Start by loading the value 5 into both the X and Y registers
-  ldx #5
-  ldy #5
+  ldx #5                        ; carrega a constante 5 no registrador x
+  ldy #5                        ; carrega a constante 5 no registrador x
 
-  ; Increment the value of X twice
+  inx                           ; registrador x++
   inx
-  inx
+  dex                           ; registrador x--
 
-  ; Decrement the value of X once
-  dex
-
-  ; Decrement the value Y twice
+  dey                           ; registrador y--
   dey
-  dey
+  iny                           ; registrador y++
 
-  ; Increment the value of Y once
-  iny
-
-  ; Since we ran 2 increments and 1 decrement on X, it should now equal 6
-  ; Since we ran 2 decrements on Y and 1 increment, it should now equal 4
-  rts
+  rts                           ; sair da subrotina atual
 .endproc
--- examples/02_LoadAndStore.s
+++ examples/02_LoadAndStore.s
@@ -2,33 +2,17 @@
 .segment "CODE"
 
 .proc Main
-  ; Inicialização de memória ZeroPage
-  ; Os primeiros 256 bytes de memória do 6502 são chamados de ZeroPage
-  ; Carrega o valor '10' no registrador X
-  ; # significa uma constante ou valor imediato
-  ; o valor sem $ ou % indica que é um valor decimal
-  ldx #10
-  ; Armazena o conteúdo do registrador X no endereço de memória $00
-  ; $ indica que o número é hexadecimal
-  ; % indica que o número é binário
-  stx $00
-  ; Immediate addressing
-  ; Só funciona com endereços de 1 byte
-  stx $01
+  ldx #10                       ; armazena o valor 10 no registrador x
+  stx $00                       ; armazena o valor de x no endereço $00
+  stx $01                       ; armazena o valor de x no endereço $01
 
-  ; Incremento do valor armazenado no endereço de memória $00
-  inc $00
-  ; Decremento do valor armazenado no endereço de memória $01
-  dec $01
+  inc $00                       ; valor dentro de $00 ++
+  dec $01                       ; valor dentro de $01 --
 
-  ; Carrega o valor armazenado no endereço de memória $00 dentro do registrador X
-  ldx $00
-  ; Armazena o conteúdo do registrador X no endereço de memória $0300
-  stx $0300
-  ; Carrega o valor armazenado no endereço de memória $01 dentro do registrador X
-  ldx $01
-  ; Armazena o conteúdo do registrador X no endereço de memória $0301
-  stx $0301
+  ldx $00                       ; carrega o valor de $00 no registrador x
+  stx $0300                     ; armazena valor de x no endereço $0300
+  ldx $01                       ; carrega o valor de $01 no registrador x
+  stx $0301                     ; armazena valor de x no endereço $0301
 
   rts
 .endproc
